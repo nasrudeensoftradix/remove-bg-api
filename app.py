2b62e0c50941bf3f370f1f562ae933750a9f8cfc
nasrudeen ansari <nasrudeen@softradix.in>
2023-08-21 13:09:58 +0530

    assing a port

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
 from rembg import remove
 from flask_cors import CORS
+import os
+
 
 
 app = Flask(__name__)
@@ -23,4 +25,5 @@ def remove_background():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
